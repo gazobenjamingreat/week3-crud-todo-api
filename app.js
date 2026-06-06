6a6a0de694f9d5084f51ebf927c781903fc41971
Gazo Benjamin Great <gazobenjamingreat@gmail.com>
2026-06-06 01:03:09 +0000

    Worked  on proper require for the environment variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Load .env variables 
 const express = require('express');
 const app = express();
 app.use(express.json()); // Parse JSON bodies
@@ -65,5 +66,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
